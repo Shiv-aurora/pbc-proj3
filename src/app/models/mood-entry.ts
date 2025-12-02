87945d20a5ddae535e9e262cefa21a6dd0586c20
“ambyrbaker” <abaker08@villanova.edu>
2025-12-02 12:48:53 -0500

    mood component created

---
+++ src/app/models/mood-entry.ts
@@ -0,0 +1,6 @@
+export interface MoodEntry {
+    date: string;        // "2021-04-16"
+    mood: string;        // "yolo", "angry", etc.
+    activities: string[]; // ["reading", "art", ...]
+  }
+  
