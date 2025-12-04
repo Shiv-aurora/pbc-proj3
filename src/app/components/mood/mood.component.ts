98037845914bcd6002831027f8c81233ecfa3b79
“ambyrbaker” <abaker08@villanova.edu>
2025-12-04 12:37:00 -0500

    msg

--- src/app/components/mood/mood.component.ts
+++ src/app/components/mood/mood.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import * as Papa from 'papaparse';
+
+interface MoodEntry {
+  date: string;
+  mood: string;
+  // add more fields to match your CSV headers
+}
 
 @Component({
-  selector: 'app-calories-chart',
+  selector: 'app-mood',                       // <app-mood></app-mood>
   standalone: true,
-  imports: [],
+  imports: [CommonModule],                    // for *ngIf, *ngFor in the template
   templateUrl: './mood.component.html',
-  styleUrl: './mood.component.scss'
+  styleUrls: ['./mood.component.scss']
 })
-export class MoodComponent {
+export class MoodComponent implements OnInit {
 
+  moodData: MoodEntry[] = [];
+  loading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadMoodData();
+  }
+
+  private loadMoodData(): void {
+    this.http.get('assets/daylio/daylio_mood.csv', { responseType: 'text' })
+      .subscribe({
+        next: (csv: string) => {
+          Papa.parse<MoodEntry>(csv, {
+            header: true,
+            skipEmptyLines: true,
+            complete: (results: Papa.ParseResult<MoodEntry>) => {
+              this.moodData = results.data;
+              this.loading = false;
+              console.log('Parsed data:', this.moodData);
+            }
+          });
+        },
+        error: (err) => {
+          console.error('Error loading CSV:', err);
+          this.loading = false;
+        }
+      });
+  }  
 }
