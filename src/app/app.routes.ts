ea1f17fee0205420fb8a95a95d00cbc83bdc64af
“ambyrbaker” <abaker08@villanova.edu>
2025-12-04 12:03:49 -0500

    new change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CaloriesComponent } from './components/calories/calories.component';
 import { MoodComponent } from './components/mood/mood.component';
+import { HabitComponent } from './habit/habit.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   // Future routes can be added here:
-  // { path: 'habits', component: HabitsComponent },
+   { path: 'habits', component: HabitComponent },
    { path: 'mood', component: MoodComponent },
   { path: 'calories', component: CaloriesComponent },
   { path: '**', redirectTo: '/dashboard' }
