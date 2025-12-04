c8b8c4c5ef1d95fe0691002f522b077f6e8932e6
“ambyrbaker” <abaker08@villanova.edu>
2025-12-04 11:50:02 -0500

    fixed mood error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CaloriesComponent } from './components/calories/calories.component';
+import { MoodComponent } from './components/mood/mood.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   // Future routes can be added here:
   // { path: 'habits', component: HabitsComponent },
-  // { path: 'mood', component: MoodComponent },
+   { path: 'mood', component: MoodComponent },
   { path: 'calories', component: CaloriesComponent },
   { path: '**', redirectTo: '/dashboard' }
 ];
