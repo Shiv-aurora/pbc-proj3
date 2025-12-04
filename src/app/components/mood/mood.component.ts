67d41ddd1035c1d81fa5415eb7267ee3481e536b
“ambyrbaker” <abaker08@villanova.edu>
2025-12-04 11:56:03 -0500

    mood tracker update

--- src/app/components/mood/mood.component.ts
+++ src/app/components/mood/mood.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-mood',
+  selector: 'app-calories-chart',
   standalone: true,
   imports: [],
   templateUrl: './mood.component.html',
